45e951e9080e8aa006e19218fc2d036fe6a04864
jayatemi <jonathanatemi@gmail.com>
2024-04-21 12:47:06 -0400

    Dev tool initial commit

---
+++ lib/atemi_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "atemi_view_tool/version"
+require_relative "atemi_view_tool/renderer"
+
+module AtemiViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
